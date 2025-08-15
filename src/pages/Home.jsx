dac35d98cf8ac4a000dae4398264241b671f9bc5
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-08-15 21:25:16 +0530

    Add files via upload
    Add animation on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
 
 const Home = () => {
+
+  useGSAP(() => {
+    const heroSplit = new SplitText('#hero-heading', { type: 'chars, words' });
+    const subtitleSplit = new SplitText('#hero-subtitle', { type: 'lines' });
+    const paraSplit = new SplitText('#hero-para', { type: 'lines' })
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: 'expo.out',
+      stagger: 0.06,
+    })
+
+    gsap.from(subtitleSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: 'expo.out',
+      stagger: 0.06,
+      delay: 1,
+    })
+
+    gsap.from(paraSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: 'expo.out',
+      stagger: 0.06,
+      delay: 1.4,
+    })
+  }, [])
+
   return (
     <div className='page-frame flex items-end sm:items-center h-[90vh]'>
       <div className=' flex flex-col gap-4'>
-        <h2 className='text-[9vw] sm:text-[10vh]'>Hi, I'm <span>Dhiraj Verma.</span></h2>
-        <h4 className='sm:text-xl'>Frontend Developer| HTML5, CSS3, JS, React.js Enthusiast</h4>
-        <p className='sm:text-8'>I create seamless, interactive web experiences that bring your ideas to life.</p>
+        <h2 id="hero-heading" className='text-[9vw] sm:text-[10vh]'>Hi, I'm <span>Dhiraj Verma.</span></h2>
+        <h4 id="hero-subtitle" className='sm:text-xl'>Frontend Developer| HTML5, CSS3, JS, React.js Enthusiast</h4>
+        <p id="hero-para" className='sm:text-8'>I create seamless, interactive web experiences that bring your ideas to life.</p>
         <div className="flex gap-3">
           <button className='bg-red-400 btn'>Download CV</button>
           <button className='btn border btn-hover'>Contact Me</button>
