0aa22d507dd93cd5d71725b8109e67ef5df9727d
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-08-20 14:41:12 +0530

    Add files via upload

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import ProjectCard from "../components/ProjectCard";
 import { projectsData } from "../constants/data";
 import gsap from "gsap";
+// import { useState } from "react";
 
 const Project = () => {
   // console.log("Data", projectsData);
@@ -16,8 +17,17 @@ const Project = () => {
     })
   }, [])
 
+  // const [selectedTech, setSelectedTech] = useState('All')
+  // const filterProject = selectedTech === 'All' ? projectsData.projects : projectsData.projects.filter(project => project.technologies.includes(selectedTech))
+
   return (
     <div className="page-frame overflow-hidden">
+      {/* <div className="flex-center gap-2">
+        <button onClick={() => setSelectedTech('React')} className="btn border btn-hover flex-center" >All</button>
+        <button onClick={() => setSelectedTech('React')} className="btn border btn-hover flex-center" >React</button>
+        <button onClick={() => setSelectedTech('React')} className="btn border btn-hover flex-center" >JS</button>
+        <button onClick={() => setSelectedTech('React')} className="btn border btn-hover flex-center" >React</button>
+      </div> */}
       <div className="w-full md:flex sm:justify-center sm:flex-wrap p-2">
         {projectsData.projects.map((item) => (
           <div id="projectCard" key={item.id} className="md:w-[50%] lg:w-[30%] p-2">
@@ -30,6 +40,18 @@ const Project = () => {
           </div>
         ))}
       </div>
+      {/* <div className="w-full md:flex sm:justify-center sm:flex-wrap p-2">
+        {filterProject.map((item) => (
+          <div id="projectCard" key={item.id} className="md:w-[50%] lg:w-[30%] p-2">
+            <ProjectCard
+              id={item.id}
+              title={item.title}
+              subtitle={item.subtitle}
+              image={item.images?.thumbnail}
+            />
+          </div>
+        ))}
+      </div> */}
     </div>
   );
 };
