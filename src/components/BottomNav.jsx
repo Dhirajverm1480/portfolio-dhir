a5b99cf84eae91fc7f4dc6b8b6cfeb35d9294454
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-08-31 22:00:36 +0530

    Add files via upload
    improve ui for mobile

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import HomeIcon from '../assets/icons/home.png';
+import CodeIcon from '../assets/icons/code-alt.png';
+import BrainIcon from '../assets/icons/brain.png';
+import PhoneIcon from '../assets/icons/phone.png';
+import BraketIcon from '../assets/icons/bracket-curly.png';
 
 const BottomNav = () => {
   return (
-    <nav className='fixed -bottom-1 w-full backdrop-blur-lg p-5 sm:hidden'>
+    <nav className='bottom-navigation'>
       <ul className="flex justify-between">
-          <NavLink to="/">
-            <span>Home</span>
+          <NavLink to="/" className='nav-link'>
+            <img src={HomeIcon} alt="" className='nav-img-icon' />
+            <span className='text-sm'>Home</span>
           </NavLink>
-          <NavLink to="/about">
-            <span>About</span>
+          <NavLink to="/about" className='nav-link'>
+            <img src={BraketIcon} alt="" className='nav-img-icon' />
+            <span className='text-sm'>About</span>
           </NavLink>
-          <NavLink to="/project">
-            <span>Project</span>
+          <NavLink to="/project" className='nav-link'>
+            <img src={CodeIcon} alt="" className='nav-img-icon' />
+            <span className='text-sm'>Project</span>
           </NavLink>
-          <NavLink to="/skills">
-            <span>Skills</span>
+          <NavLink to="/skills" className='nav-link'>
+            <img src={BrainIcon} alt="" className='nav-img-icon' />
+            <span className='text-sm'>Skills</span>
           </NavLink>
-          <NavLink to="/contact">
-            <span>Contact</span>
+          <NavLink to="/contact" className='nav-link'>
+            <img src={PhoneIcon} alt="" className='nav-img-icon' />
+            <span className='text-sm'>Contact</span>
           </NavLink>
         </ul>
     </nav>
