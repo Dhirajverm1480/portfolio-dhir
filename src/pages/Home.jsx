fb9dff31e214f455e6a98272d0dbb246cb9c72bd
Dhirajverm1480 <dhirajv418@gmail.com>
2026-01-29 21:35:19 +0530

    animation improvement in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,16 +36,27 @@ const Home = () => {
       stagger: 0.06,
       delay: 1.4,
     })
-  }, [])
+
+    return () => {
+      heroSplit.revert();
+      subtitleSplit.revert();
+      paraSplit.revert();
+    };
+  }, {scope: "#page-frame" })
 
   return (
-    <div className='page-frame flex items-center h-[calc(100vh-130px)] sm:h-[calc(100vh-64px)] lg:h-[calc(100vh-64px)] xl:overflow-hidden'>
+    <div id="page-frame" className='page-frame flex items-center h-[calc(100vh-130px)] sm:h-[calc(100vh-64px)] lg:h-[calc(100vh-64px)] xl:overflow-hidden'>
       <div className='w-full flex flex-col gap-4'>
-        <h2 id="hero-heading" className='text-[9vw] lg:text-[5vw]'>Hi, I'm <span>Dhiraj Verma.</span></h2>
-        <h4 id="hero-subtitle" className='sm:text-xl'>Frontend Developer| HTML5, CSS3, JS, React.js Enthusiast</h4>
-        <p id="hero-para" className='sm:text-8'>I create seamless, interactive web experiences that bring your ideas to life.</p>
+        <h1 id="hero-heading" className='text-[9vw] lg:text-[5vw]'>Hi, I'm <span>Dhiraj Verma.</span></h1>
+        <h2 id="hero-subtitle" className='sm:text-xl'>Frontend Developer| HTML5, CSS3, JS, React.js Enthusiast</h2>
+        <p id="hero-para" className='sm:text-lg'>I create seamless, interactive web experiences that bring your ideas to life.</p>
         <div className="flex gap-3">
-          <a href={Resume} className='bg-red-400 btn flex-center' target="_blank" >Download CV</a >
+          <a
+            href={Resume}
+            className='bg-red-400 btn flex-center'
+            target="_blank"
+            rel="noopener noreferrer"
+          >Download CV</a >
           <Link to={'/about'} className='btn border btn-hover flex-center'>About Me</Link>
         </div>
       </div>
